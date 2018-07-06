1c7c4e24b01437b5b8e2105707dfb738a850f3b0
shifuddin <shifuddin.masud@gmail.com>
2018-07-06 23:03:42 +0200

    save

--- orch/engine_manager.py
+++ orch/engine_manager.py
@@ -16,40 +16,78 @@ class EngineManager():
         self.engine_list = []
     
     def add_agent_db(self, session, resource):
-        
+        '''
+        insert new agen / fog node to database
+        '''
         ag = Agent(ip=resource.get('ip'), building = resource.get('building'),
                    postal_code = resource.get('postal_code'), city=resource.get('city'),
                    cpu= resource.get('cpu'), mem=resource.get('mem'))
         session.add(ag)
         session.commit()
+        
+        
     def update_agent(self, old_agent, resource, session):
+        
+        '''
+        update capabilities of  a fog node for example cpu, memory
+        '''
         old_agent.building = resource.get('building')
         old_agent.postal_code= resource.get('postal_code')
         old_agent.city = resource.get('city')
         old_agent.cpu = resource.get('cpu')
         old_agent.mem = resource.get('mem')
         session.commit()
-    def check_db(self, resource, session):
         
+        
+    def check_db(self, resource, session):
+        '''
+        check weather a agent / resource already in the database
+        if yes, just update the properties of the resource
+        if no, add the resource to database, check wheather there is a engine for that region
+        '''
         old_agent = session.query(Agent).filter_by(ip = resource.get('ip')).first()
         if old_agent is None:
             self.add_agent_db(session, resource)
+            self.check_engine(resource.get('city') + '_' + resource.get('postal_code'), resource)
         else:
             self.update_agent(old_agent, resource, session)
+            self.pass_updated_resource(resource.get('city') + '_' + resource.get('postal_code'), resource)
     
-    def create_engine(self, engine_name, resource):
-        
+    def check_engine(self, engine_name, resource):
+        '''
+        Check wheather a engine already exists
+        if yes, just give the resource to region manager of that engine.
+        '''
         for orch_eng in self.engine_list:
             if engine_name == orch_eng.engine_name:
                 orch_eng.add_resource(resource)
                 print ('Engine already exists')
                 return
         
+        self.create_new_engine(engine_name, resource)
+    
+    def pass_updated_resource(self, engine_name, resource):
+        '''
+        pass update resource to a particular engine
+        '''
+        for orch_eng in self.engine_list:
+            if engine_name == orch_eng.engine_name:
+                orch_eng.add_resource(resource)
+                return
+    
+    def create_new_engine(self, engine_name, resource):
+        '''
+        Create new engine and pass resource to that engine
+        '''
         orch_en = Engine(engine_name)
         self.engine_list.append(orch_en)
         orch_en.add_resource(resource)
         print (len(self.engine_list))
+        
     def place_resource(self, resources):
+        '''
+        Take resources from resouce pool to be placed to region manager of a engine
+        '''
         session = self.Session()
         {self.check_db(resource, session) for resource in resources}
         
--- orch/resource_pool.py
+++ orch/resource_pool.py
@@ -2,7 +2,9 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+Resource pool accepts new resource at startup time
+It accepts new resource at runtime
+It also accepts resource for update
 """
 from engine_manager import EngineManager
 from random import randint, choice
@@ -13,18 +15,32 @@ class ResourcePool():
         self.callback = callback
         
     def accept_resource(self, resource):
+        '''
+        accept a new resource
+        @param resource: propertifes of fog node as dict 
+        '''
         self.callback([resource])
     
     def accept_bulk_resources(self, resources):
+        '''
+        accept a list of resources
+        @param resources: list of properties of fog nodes.
+        '''
         self.callback(resources)
         
 def generate_address():
+    '''
+    generate space seperated three words address
+    '''
     first_part = ''.join(choice(string.ascii_letters) for _ in range(6))
     second_part = ''.join(choice(string.ascii_letters) for _ in range(6) )
 
     return first_part + ' ' + second_part + ' ' +  str(randint(1, 50))
+
 def generate_resources():
-    
+    '''
+    generate properties of fog node randomly in a bulk amount 
+    '''
     resources = []
     for number in range (1 , 20):
         resource = {
@@ -37,18 +53,26 @@ def generate_resources():
         }
         resources.append(resource)
     return resources
-engine = EngineManager()
-resourcepool = ResourcePool(engine.place_resource)
-# add single resource
-resource = {
-        'ip': '10.10.10.14',
+
+def generate_single_resource():
+    '''
+    create static single resource properties
+    '''
+    resource = {
+        'ip': '113.2.101.79',
         'building': 'Hans leipelt str. 7',
         'postal_code': 80807,
         'city': 'Munich',
         'cpu': 1000,
         'mem': 3000
         }
-resourcepool.accept_resource(resource)
+    return resource
+    
+engine = EngineManager()
+resourcepool = ResourcePool(engine.place_resource)
+
+# add single resource
+resourcepool.accept_resource(generate_single_resource())
         
 # add bulk resources
 resourcepool.accept_bulk_resources(generate_resources())
